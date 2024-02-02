5a6b61d6ce6f3f33fbfbaa22860dca069e1ed731
Ammar Askar <ammar@ammaraskar.com>
2024-02-01 23:32:45 -0500

    Disable twitter embedding.

--- events_bot/__main__.py
+++ events_bot/__main__.py
@@ -10,6 +10,7 @@ event_storage = events_manager.NewlineDelimitedJsonEventStorage("db.json")
 calendar = calendar.Calendar(secrets["APP_SCRIPT_URL"], secrets["APP_SCRIPT_TOKEN"])
 
 client = bot.EventsBotClient(event_storage=event_storage, calendar=calendar)
-tweet_fixer.register_bot_for_tweet_fixing(client)
+# Temporarily disabled, twitter may have fixed embeds.
+#tweet_fixer.register_bot_for_tweet_fixing(client)
 
 client.run(secrets["DISCORD_BOT_TOKEN"])
